ee6e035dbb9153f7cec93ec72966fec539790ff5
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-06-17 15:15:44 +0100

    Extract player functions to class, remove bullet references

--- main.lua
+++ main.lua
@@ -1,45 +1,18 @@
-local player = {}
-local bullets = {}
-
-local Bullet = {
-    w = 10,
-    h = 10
-}
+local Player = require('src.entities.player')
 
 function love.load()
-    player.w = 60
-    player.h = 20
-    player.x = love.graphics.getWidth() / 2 - player.w / 2
-    player.y = love.graphics.getHeight() - 40
-    player.speed = 200
-    player.fire = function()
-        local bullet = { 
-            x = player.x + player.w / 2 - Bullet.w / 2,
-            y = player.y,
-            speed = -100
-        }
-        table.insert(bullets, bullet)
-    end
+    player = Player:new{
+        x = love.graphics.getWidth() / 2,
+        y = love.graphics.getHeight() - 40
+    }
 end
 
 function love.update(dt)
-    local dx = 0
-    if love.keyboard.isDown('left') or love.keyboard.isDown('a') then 
-        dx = dx - 1 
-    end
-    if love.keyboard.isDown('right') or love.keyboard.isDown('d') then 
-        dx = dx + 1 
-    end
-    player.x = player.x + dx * dt * player.speed
-    updateBullets(dt)
+    player:update(dt)
 end
 
 function love.draw()
-    drawBullets()
-    love.graphics.setColor(0, 1, 0)
-    love.graphics.rectangle('fill', player.x, player.y, player.w, player.h)
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.print('Number of bullets: ' .. tostring(#bullets), 10, 10)
+    player:draw()
 end
 
 function love.keypressed(key)
@@ -47,22 +20,6 @@ function love.keypressed(key)
         love.event.quit()
     end
     if key == 'space' then
-        player.fire()
-    end
-end
-
-function updateBullets(dt)
-    for i, b in ipairs(bullets) do
-        b.y = b.y + b.speed * dt
-        if b.y <= Bullet.h then
-            table.remove(bullets, i)
-        end
-    end
-end
-
-function drawBullets()
-    love.graphics.setColor(1, 1, 1)
-    for _, b in pairs(bullets) do
-        love.graphics.rectangle('fill', b.x, b.y, Bullet.w, Bullet.h)
+        player:fire()
     end
 end
---
+++ src/entities/player.lua
@@ -0,0 +1,44 @@
+local Player = {}
+
+local width = 60
+local height = 20
+local speed = 200
+local colour = { 0, 1, 0 }
+
+Player.__index = Player
+
+function Player:new(params)
+    local inst = {}
+    
+    inst.w, inst.h = width, height
+    inst.x = (params.x or 0) - inst.w / 2
+    inst.y = (params.y or 0) - inst.h / 2
+    inst.colour = params.colour or colour
+    inst.speed = params.speed or speed
+    
+    setmetatable(inst, self)
+
+    return inst
+end
+
+function Player:update(dt)
+    local dx = 0
+    if love.keyboard.isDown('left') or love.keyboard.isDown('a') then 
+        dx = dx - 1 
+    end
+    if love.keyboard.isDown('right') or love.keyboard.isDown('d') then 
+        dx = dx + 1 
+    end
+    self.x = self.x + dx * dt * self.speed
+end
+
+function Player:draw()
+    love.graphics.setColor(unpack(self.colour))
+    love.graphics.rectangle('fill', self.x, self.y, self.w, self.h)
+end
+
+function Player:fire()
+    print('fire!')
+end
+
+return Player
